32ee2ca9c7e9c62d3f0a836d744419625034068f
Spicy Ramen <56414768+yhuen24@users.noreply.github.com>
2021-05-03 20:55:39 +0100

    Added Login Page

--- main.py
+++ main.py
@@ -34,5 +34,10 @@ def signup():
     return render_template("signup.html")
 
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
